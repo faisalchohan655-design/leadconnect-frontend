4a84c6e33d9ce11b1555238dd291ffff755ac002
faisalchohan655-design <faisalchohan655@gmail.com>
2026-06-13 16:42:41 +0500

    Add WhatsAppOutreach component with ComingSoon

---
+++ src/components/WhatsAppOutreach.jsx
@@ -0,0 +1,4 @@
+// WhatsAppOutreach.jsx
+import ComingSoon from './ComingSoon';
+const WhatsAppOutreach = () => <ComingSoon feature="WhatsApp Outreach" description="Bulk WhatsApp messaging and automation." />;
+export default WhatsAppOutreach;
